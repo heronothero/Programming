650c6ea350c437f135313290b27f12090d756f9f
heronothero <mazayadasha@gmail.com>
2024-08-27 02:50:48 +0700

    Created Point2D class (readonly)

---
+++ Programming/Model/Point2D.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public class Point2D
+    {
+        public double X { get; } 
+        public double Y { get; }
+        public Point2D (double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
