34d4c878499cc67815c27893c07a27d57b98c892
heronothero <mazayadasha@gmail.com>
2024-08-29 04:45:47 +0700

    Minor changes

--- Programming/View/MainForm.cs
+++ Programming/View/MainForm.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
