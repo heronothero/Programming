239c203ecd11de41b3b1089dd4ada95f8927f01b
ohhgod244-admin <mazikova.darya@gmail.com>
2026-03-21 22:55:29 +0700

    Создание класса проверки значений строки
    Создание динамического класса для проверки значений

---
+++ src/ObjectOrientedPractics/Services/ValueValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Services
+{
+    /// <summary>
+    /// Создание класса обработки значений
+    /// </summary>
+    public static class ValueValidator
+    {
+        /// <summary>
+        /// Установка значений для переменной
+        /// </summary>
+        /// <param name="value">Значение переменной</param>
+        /// <param name="maxLength">Максимальная длинна, разрешенная условием</param>
+        /// <param name="propertyName">Имя переменной</param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void AssertStringOnLength (string value, int maxLength, string propertyName)
+        {
+            if (value == null)
+                throw new ArgumentException($"{propertyName} не может быть null.");
+
+            if (value.Length > maxLength)
+                throw new ArgumentException($"{propertyName} должен быть не длиннее {maxLength} символов.");
+        }
+    }
+}
