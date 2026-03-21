f2e3a5b896374943d7b7337300562ebec2d42100
ohhgod244-admin <mazikova.darya@gmail.com>
2026-03-21 22:25:53 +0700

    Create IdGenerator class
    Counts id

---
+++ src/ObjectOrientedPractics/Services/IdGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Services
+{
+    /// <summary>
+    /// Class for id
+    /// </summary>
+    public static class IdGenerator
+    {
+        /// <summary>
+        /// Set a specific number
+        /// </summary>
+        private static int _currentId = 1;
+
+        /// <summary>
+        /// Counts id
+        /// </summary>
+        /// <returns>next id will be 1 more</returns>
+        public static int GetNextId()
+        {
+            return _currentId++;
+        }
+    }
+}
