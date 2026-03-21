090e04859c21561f7faa9e88a2d2d25035c8d1e8
ohhgod244-admin <mazikova.darya@gmail.com>
2026-03-21 23:07:23 +0700

    Апдейт валидации
    Централизировала логику и добавила проверку числовых значений

--- src/ObjectOrientedPractics/Services/ValueValidator.cs
+++ src/ObjectOrientedPractics/Services/ValueValidator.cs
@@ -17,7 +17,7 @@ namespace ObjectOrientedPractics.Services
         /// <param name="value">Значение переменной</param>
         /// <param name="maxLength">Максимальная длинна, разрешенная условием</param>
         /// <param name="propertyName">Имя переменной</param>
-        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentException">Выбрасывается, если значение не удовлетворяет условиям</exception>
         public static void AssertStringOnLength (string value, int maxLength, string propertyName)
         {
             if (value == null)
@@ -25,6 +25,24 @@ namespace ObjectOrientedPractics.Services
 
             if (value.Length > maxLength)
                 throw new ArgumentException($"{propertyName} должен быть не длиннее {maxLength} символов.");
+        }
+
+        /// <summary>
+        /// Установка численных значений для переменной
+        /// </summary>
+        /// <typeparam name="T">Тип числа (должен реализовывать IComparable)</typeparam>
+        /// <param name="value">Значение переменной</param>
+        /// <param name="min">Минимальное разрешенное значение</param>
+        /// <param name="max">Максимальное разрешенное значение</param>
+        /// <param name="propertyName">Имя переменной</param>
+        /// <exception cref="ArgumentOutOfRangeException">Выбрасывается, если значение не удовлетворяет условиям</exception>
+        public static void AssertNumberInRange<T>(T value, T min, T max, string propertyName) where T : IComparable<T>
+        {
+            if (value.CompareTo(min) < 0 || value.CompareTo(max) > 0)
+            {
+                throw new ArgumentOutOfRangeException(propertyName, value,
+                    $"{propertyName} должен быть в диапазоне от {min} до {max}.");
+            }
         }
     }
 }
