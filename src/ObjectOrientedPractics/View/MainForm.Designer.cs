8b11013317376313e493aa7a1356a97c32d58788
ohhgod244-admin <mazikova.darya@gmail.com>
2026-03-22 12:05:28 +0700

    В главную форму добавлен пользовательский элемент управления

--- src/ObjectOrientedPractics/View/MainForm.Designer.cs
+++ src/ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.ItemsTabPage = new System.Windows.Forms.TabPage();
+            this.CustomersTabPage = new System.Windows.Forms.TabPage();
+            this.itemsTabControl = new ObjectOrientedPractics.View.Tabs.ItemsTab();
+            this.tabControl1.SuspendLayout();
+            this.ItemsTabPage.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.ItemsTabPage);
+            this.tabControl1.Controls.Add(this.CustomersTabPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(818, 656);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // ItemsTabPage
+            // 
+            this.ItemsTabPage.Controls.Add(this.itemsTabControl);
+            this.ItemsTabPage.Location = new System.Drawing.Point(4, 25);
+            this.ItemsTabPage.Name = "ItemsTabPage";
+            this.ItemsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ItemsTabPage.Size = new System.Drawing.Size(810, 627);
+            this.ItemsTabPage.TabIndex = 0;
+            this.ItemsTabPage.Text = "ItemsTab";
+            this.ItemsTabPage.UseVisualStyleBackColor = true;
+            // 
+            // CustomersTabPage
+            // 
+            this.CustomersTabPage.Location = new System.Drawing.Point(4, 25);
+            this.CustomersTabPage.Name = "CustomersTabPage";
+            this.CustomersTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.CustomersTabPage.Size = new System.Drawing.Size(810, 624);
+            this.CustomersTabPage.TabIndex = 1;
+            this.CustomersTabPage.Text = "CustomersTab";
+            this.CustomersTabPage.UseVisualStyleBackColor = true;
+            // 
+            // itemsTabControl
+            // 
+            this.itemsTabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.itemsTabControl.Location = new System.Drawing.Point(3, 3);
+            this.itemsTabControl.Name = "itemsTabControl";
+            this.itemsTabControl.Size = new System.Drawing.Size(804, 621);
+            this.itemsTabControl.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(818, 656);
+            this.Controls.Add(this.tabControl1);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.tabControl1.ResumeLayout(false);
+            this.ItemsTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage ItemsTabPage;
+        private System.Windows.Forms.TabPage CustomersTabPage;
+        private View.Tabs.ItemsTab itemsTabControl;
     }
 }
 
