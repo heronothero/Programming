1bcc469025421fa4bbecb4f31988630592bc03cc
ohhgod244-admin <mazikova.darya@gmail.com>
2026-03-24 20:59:57 +0700

    Создание класса корзины и общей стоимости товаров в ней

---
+++ src/ObjectOrientedPractics/Model/Cart.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Класс корзины
+    /// </summary>
+    public class Cart
+    {
+        /// <summary>
+        /// Список товаров
+        /// </summary>
+        private List<Item> _items = new List<Item>();
+
+        /// <summary>
+        /// Список товаров в корзине
+        /// </summary>
+        public List<Item> Items
+        {
+            get => _items;
+            set => _items = value ?? new List<Item>();
+        }
+
+        /// <summary>
+        /// Общая стоимость товаров
+        /// </summary>
+        public decimal Amount
+        {
+            get
+            {
+                if (_items.Count == 0 || _items == null)
+                    return 0;
+                decimal sum = 0;
+
+                foreach (var item in _items)
+                {
+                    sum += item.Cost;
+                }
+
+                return sum;
+            }
+        }
+    }
+}
