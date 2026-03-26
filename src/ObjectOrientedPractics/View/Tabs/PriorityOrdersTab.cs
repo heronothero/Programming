07495437dfb584e5026ffe20797abfe63704db49
ohhgod244-admin <mazikova.darya@gmail.com>
2026-03-26 21:31:06 +0700

    Обновление приоритетных заказов

--- src/ObjectOrientedPractics/View/Tabs/PriorityOrdersTab.cs
+++ src/ObjectOrientedPractics/View/Tabs/PriorityOrdersTab.cs
@@ -16,5 +16,55 @@ namespace ObjectOrientedPractics.View.Tabs
         {
             InitializeComponent();
         }
+
+        private void IdTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreatedTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StatusComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeliveryTimeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addressControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OrderItemsTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddItemButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RemoveItemButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SumLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClearOrderButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
