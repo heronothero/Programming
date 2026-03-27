27a3779625d20c6e8f15fd77838212c57fe063cb
ohhgod244-admin <mazikova.darya@gmail.com>
2026-03-27 22:05:17 +0700

    Создание скидок и скидочных баллов

---
+++ src/ObjectOrientedPractics/Model/Discounts/PointsDiscount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model.Discounts
+{
+    public class PointsDiscount
+    {
+        private int _points;
+
+        public int Points
+        {
+            get => _points;
+            private set => _points = value < 0 ? 0 : value;
+        }
+
+        public string Info => $"Накопительная – {Points} баллов";
+
+        public double Calculate(List<Item> items)
+        {
+            double total = (double)items.Sum(i => i.Cost); //Можно ли так явно преобразовывать?
+            double maxDiscount = total * 0.3;
+
+            return Math.Min(Points, maxDiscount);
+        }
+
+        public double Apply(List<Item> items)
+        {
+            double discount = Calculate(items);
+
+            Points -= (int)Math.Ceiling(discount);
+
+            return discount;
+        }
+
+        public void Update(List<Item> items)
+        {
+            double total = (double)items.Sum(i => i.Cost);
+
+            int earnedPoints = (int)Math.Ceiling(total * 0.1);
+            Points += earnedPoints;
+        }
+    }
+}
