5249a704b4d5a584260a2853b630b74999937700
ohhgod244-admin <mazikova.darya@gmail.com>
2026-03-28 03:56:22 +0700

    Делегаты.

---
+++ src/ObjectOrientedPractics/Services/DataTools.cs
@@ -0,0 +1,48 @@
+﻿using ObjectOrientedPractics.Model;
+using ObjectOrientedPractics.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Services
+{
+    public static class DataTools
+    {
+        public static List<Item> Filter(List<Item> items, Func<Item, bool> filter)
+        {
+            List<Item> result = new List<Item>();
+
+            foreach (var item in items)
+            {
+                if (filter(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
+        public static List<Item> Sort(List<Item> items, Func<List<Item>, List<Item>> sorter)
+        {
+            return sorter(items);
+        }
+
+        public static List<Item> SortByName(List<Item> items)
+        {
+            return items.OrderBy(i => i.Name).ToList();
+        }
+
+        public static List<Item> SortByCostAsc(List<Item> items)
+        {
+            return items.OrderBy(i => i.Cost).ToList();
+        }
+
+        public static List<Item> SortByCostDesc(List<Item> items)
+        {
+            return items.OrderByDescending(i => i.Cost).ToList();
+        }
+    }
+}
