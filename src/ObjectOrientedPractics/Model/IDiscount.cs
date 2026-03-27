d8e4d5d88979f4af72f3a94b329f8f30374a6ceb
ohhgod244-admin <mazikova.darya@gmail.com>
2026-03-27 22:05:30 +0700

    Создание интерфейса для скидок

---
+++ src/ObjectOrientedPractics/Model/IDiscount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Интерфейс для всех типов скидок
+    /// </summary>
+    public interface IDiscount
+    {
+        /// <summary>
+        /// Возвращает информацию о скидке
+        /// </summary>
+        string Info { get; }
+
+        /// <summary>
+        /// Рассчитывает размер скидки без её применения
+        /// </summary>
+        double Calculate(List<Item> items);
+
+        /// <summary>
+        /// Применяет скидку и возвращает её размер
+        /// </summary>
+        double Apply(List<Item> items);
+
+        /// <summary>
+        /// Обновляет состояние скидки после покупки
+        /// </summary>
+        void Update(List<Item> items);
+    }
+}
