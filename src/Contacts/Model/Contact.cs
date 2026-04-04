61ff3b761ae1b5b204f8c4fc781c8be87ef7637e
ohhgod244-admin <mazikova.darya@gmail.com>
2026-04-05 01:28:16 +0700

    Создание класса Contacts
    1 конструктор без параметров для создания пустого объекта, 2 - чтобы задать данные сразу

---
+++ src/Contacts/Model/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Model
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Contact() { }
+
+        /// <summary>
+        /// Конструктор с параметрами для инициализации свойств контакта
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="phone">Номер телефона</param>
+        /// <param name="email">Электронная почта</param>
+        public Contact(string name, string phone, string email)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+        }
+    }
+}
