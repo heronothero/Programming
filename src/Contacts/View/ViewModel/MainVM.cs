d2f4918daafb6801351d7a2c925202b5ca7e3a6e
ohhgod244-admin <mazikova.darya@gmail.com>
2026-04-06 02:30:26 +0700

    Добавление функционала кнопок и команд

--- src/Contacts/View/ViewModel/MainVM.cs
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using View.Model;
 using View.Model.Services;
 
@@ -12,64 +14,155 @@ namespace View.ViewModel
 {
     public class MainVM : INotifyPropertyChanged
     {
-        private string _name;
-        private string _phoneNumber;
-        private string _email;
+        private bool _isEditing;
+        private bool _isAdding;
+        private List<Contact> _allContacts;
+        private string _searchText;
+        private ContactSerializer _serializer;
 
-        public Contact Contact { get; set; }
+        public ObservableCollection<Contact> Contacts { get; set; }
 
-        public string Name
+        private Contact _selectedContact;
+
+        public Contact SelectedContact
         {
-            get => _name;
+            get => _selectedContact;
             set
             {
-                _name = value;
+                _selectedContact = value;
                 OnPropertyChanged();
-                UpdateContact();
+
+                ((RelayCommand)EditCommand)?.RaiseCanExecuteChanged();
+                ((RelayCommand)RemoveCommand)?.RaiseCanExecuteChanged();
             }
         }
 
-        public string PhoneNumber
+        /// <summary>
+        /// Проверка на редактирование контактных данных
+        /// </summary>
+        public bool IsEditing
         {
-            get => _phoneNumber;
-            set
-            {
-                _phoneNumber = value;
-                OnPropertyChanged();
-                UpdateContact();
-            }
+            get => _isEditing;
+            set { _isEditing = value; OnPropertyChanged(); }
         }
 
-        public string Email
+        /// <summary>
+        /// Проверка на добавление данных контакта
+        /// </summary>
+        public bool IsAdding
         {
-            get => _email;
-            set
-            {
-                _email = value;
-                OnPropertyChanged();
-                UpdateContact();
-            }
+            get => _isAdding;
+            set { _isAdding = value; OnPropertyChanged(); }
         }
+
+        // Команды
+        public ICommand ApplyCommand { get; set; }
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand RemoveCommand { get; set; }
 
         public SaveCommand SaveCommand { get; set; }
         public LoadCommand LoadCommand { get; set; }
 
-        private ContactSerializer _serializer;
-
+        /// <summary>
+        /// Загрузка основной логики (команд) и компонентов
+        /// </summary>
         public MainVM()
         {
-            Contact = new Contact();
+            Contacts = new ObservableCollection<Contact>();
+
             _serializer = new ContactSerializer();
+
+            _allContacts = _serializer.Load();
+
+            if (_allContacts == null)
+                _allContacts = new List<Contact>();
+
+            Contacts = new ObservableCollection<Contact>(_allContacts);
 
             SaveCommand = new SaveCommand(this, _serializer);
             LoadCommand = new LoadCommand(this, _serializer);
+
+            ApplyCommand = new RelayCommand(Apply);
+            AddCommand = new RelayCommand(Add);
+            EditCommand = new RelayCommand(Edit, CanEdit);
+            RemoveCommand = new RelayCommand(Remove, CanRemove);
         }
 
-        private void UpdateContact()
+        private void Add(object obj)
         {
-            Contact.Name = Name;
-            Contact.Phone = PhoneNumber;
-            Contact.Email = Email;
+            var newContact = new Contact();
+
+            _allContacts.Add(newContact);
+            SelectedContact = newContact;
+
+            FilterContacts();
+
+            IsAdding = true;
+            IsEditing = false;
+        }
+
+        private void Edit(object obj)
+        {
+            if (SelectedContact == null) return;
+
+            IsEditing = true;
+            IsAdding = false;
+        }
+
+        private void Apply(object obj)
+        {
+            if (IsAdding)
+            {
+                Contacts.Add(SelectedContact);
+            }
+
+            IsAdding = false;
+            IsEditing = false;
+
+            _serializer.Save(Contacts.ToList());
+        }
+
+        private void Remove(object obj)
+        {
+            if (SelectedContact == null) return;
+
+            _allContacts.Remove(SelectedContact);
+
+            SelectedContact = null;
+
+            FilterContacts();
+        }
+
+        /// <summary>
+        /// Проверка на возможность редактирования контакта
+        /// </summary>
+        private bool CanEdit(object obj)
+        {
+            Console.WriteLine(SelectedContact);
+            return SelectedContact != null;
+        }
+
+        /// <summary>
+        /// Проверка на возможность удаления контакта
+        /// </summary>
+        private bool CanRemove(object obj)
+        {
+            return SelectedContact != null;
+        }
+
+        /// <summary>
+        /// Поиск контакта
+        /// </summary>
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                _searchText = value;
+                OnPropertyChanged();
+                FilterContacts();
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -77,6 +170,25 @@ namespace View.ViewModel
         protected void OnPropertyChanged([CallerMemberName] string prop = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private void FilterContacts()
+        {
+            if (string.IsNullOrWhiteSpace(SearchText))
+            {
+                Contacts = new ObservableCollection<Contact>(_allContacts);
+            }
+            else
+            {
+                var filtered = _allContacts
+                    .Where(c => c.Name != null &&
+                                c.Name.ToLower().Contains(SearchText.ToLower()))
+                    .ToList();
+
+                Contacts = new ObservableCollection<Contact>(filtered);
+            }
+
+            OnPropertyChanged(nameof(Contacts));
         }
     }
 }
