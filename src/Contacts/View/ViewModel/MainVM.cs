ce1ed6707918cd1e101bca60ca8b86e1a1ef2591
ohhgod244-admin <mazikova.darya@gmail.com>
2026-04-05 23:59:52 +0700

    Создание связки с объектами
    Реализует изменения данных в реальном времени (ввод в левой панели -> изменения в правой панели), сохранение и выгрузка данных и их отображение

---
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using View.Model;
+using View.Model.Services;
+
+namespace View.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _phoneNumber;
+        private string _email;
+
+        public Contact Contact { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+                UpdateContact();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                _phoneNumber = value;
+                OnPropertyChanged();
+                UpdateContact();
+            }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+                UpdateContact();
+            }
+        }
+
+        public SaveCommand SaveCommand { get; set; }
+        public LoadCommand LoadCommand { get; set; }
+
+        private ContactSerializer _serializer;
+
+        public MainVM()
+        {
+            Contact = new Contact();
+            _serializer = new ContactSerializer();
+
+            SaveCommand = new SaveCommand(this, _serializer);
+            LoadCommand = new LoadCommand(this, _serializer);
+        }
+
+        private void UpdateContact()
+        {
+            Contact.Name = Name;
+            Contact.Phone = PhoneNumber;
+            Contact.Email = Email;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
