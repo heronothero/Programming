2df0a080b6396187b558b43473241db235222f28
ohhgod244-admin <mazikova.darya@gmail.com>
2026-04-05 23:57:17 +0700

    Создание системы для работы с файлом
    Файл в формате JSON, в котором сохраняются данные при нажатии на кнопку, а также их выгрузка, указание пути и названия файла

---
+++ src/Contacts/View/Model/Services/ContactSerializer.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace View.Model.Services
+{
+    public class ContactSerializer
+    {
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// Создание файла для работы с данными контактов
+        /// </summary>
+        public ContactSerializer()
+        {
+            FilePath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                "Contacts",
+                "contacts.json"
+            );
+        }
+
+        /// <summary>
+        /// Сохранение данных контакта
+        /// </summary>
+        public void Save(Contact contact)
+        {
+            var dir = Path.GetDirectoryName(FilePath);
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            var json = JsonConvert.SerializeObject(contact, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(FilePath, json);
+        }
+
+        /// <summary>
+        /// Возвращает предыдущий сохраненный контакт (после предыдущего запуска)
+        /// </summary>
+        public Contact Load()
+        {
+            if (!File.Exists(FilePath))
+                return new Contact();
+
+            var json = File.ReadAllText(FilePath);
+            return JsonConvert.DeserializeObject<Contact>(json);
+        }
+    }
+}
