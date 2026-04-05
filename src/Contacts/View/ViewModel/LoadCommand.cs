cb3e78bc9dd4bd541d3a71b82cca5c582e02535a
ohhgod244-admin <mazikova.darya@gmail.com>
2026-04-05 23:57:54 +0700

    Логика для кнопок в приложении
    Реализует функцию сохранения и загрузки данных в приложении при взаимодействии с кнопками

---
+++ src/Contacts/View/ViewModel/LoadCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using View.Model.Services;
+
+namespace View.ViewModel
+{
+    public class LoadCommand : ICommand
+    {
+        private MainVM _vm;
+        private ContactSerializer _serializer;
+
+        public LoadCommand(MainVM vm, ContactSerializer serializer)
+        {
+            _vm = vm;
+            _serializer = serializer;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            var contact = _serializer.Load();
+
+            _vm.Name = contact.Name;
+            _vm.PhoneNumber = contact.Phone;
+            _vm.Email = contact.Email;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
